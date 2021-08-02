1fa57e12dd84274be5fa5366db437d89abac3fb6
rohitkolankar <rohitkolankar7@gmail.com>
2021-08-02 14:46:11 +0530

    added sqaure function

--- math.py
+++ math.py
@@ -1,1 +1,1 @@
-#add functiondef add(x,y):    return (x+y)#sub functiondef subtract(x,y):    pass#multiply functiondef multiply(x,y):    pass#div functiondef divide(x,y):    pass
+#add functiondef add(x,y):    return (x+y)#sub functiondef subtract(x,y):    pass#multiply functiondef multiply(x,y):    pass#div functiondef divide(x,y):    passdef square(x):    return x*x
